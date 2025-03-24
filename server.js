46ec738e12a56eeb5c86ecf0ca7d5ab505cef24c
mykel68 <olowookeremicheal9568@gmail.com>
2025-03-24 12:04:30 +0100

    fix:added dotenv package

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const jobRoutes = require('./routes/job.routes');
 const logger = require('./middlewares/logger');
+const { PORT } = require('./config/env');
+
 
 // Global middleware
 app.use(express.json());
@@ -10,5 +12,16 @@ app.use(logger);
 // API routes
 app.use('/api/jobs', jobRoutes);
 
-const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('Welcome... Hello World');
+});
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`,
+    });
+});
+
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
